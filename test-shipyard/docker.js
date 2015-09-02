509cf3798292f5c085774ffa4e56dcaa871dfbd8
chengjt <chengjt@csdn.net>
2015-09-02 14:36:58 +0800

    add shipyard test case

---
+++ test-shipyard/docker.js
@@ -0,0 +1,410 @@
+/*jshint -W030 */
+
+var chai = require('chai');
+var expect =chai.expect;
+var assert=chai.assert; 
+var docker = require('./spec_helper').docker;
+
+var testImage = 'ubuntu:14.04';
+
+describe("#docker",function(){
+  describe("#ping",function(){
+      var server="cjt-local";
+      it("ping server "+server,function(done){
+        this.timeout(30000);
+        docker.ping(server,function(err,data){
+            expect(err).to.be.null;
+            //console.log(data)
+            assert.equal(data,"OK","ping "+server +" result should be Ok");
+            done();
+        });
+      });
+  });
+});
+
+
+
+
+
+// describe("#docker", function() {
+
+//   describe("#checkAuth", function() {
+//     it("should fail auth", function(done) {
+//       this.timeout(15000);
+
+//       function handler(err, data) {
+//         expect(err).not.to.be.null;
+//         done();
+//       }
+
+//       docker.checkAuth({username: 'xpto', password: 'dang', email: 'xpto@pxpto.pt'}, handler);
+//     });
+//   });
+
+//   describe("#buildImage", function() {
+//     it("should build image from file", function(done) {
+//       this.timeout(60000);
+
+//       function handler(err, stream) {
+//         expect(err).to.be.null;
+//         expect(stream).to.be.ok;
+
+//         stream.pipe(process.stdout, {end: true});
+
+//         stream.on('end', function() {
+//           done();
+//         });
+//       }
+
+//       docker.buildImage('./test/test.tar', {}, handler);
+//     });
+
+//     it("should build image from readable stream", function(done) {
+//       this.timeout(60000);
+
+//       function handler(err, stream) {
+//         expect(err).to.be.null;
+//         expect(stream).to.be.ok;
+
+//         stream.pipe(process.stdout, {end: true});
+
+//         stream.on('end', function() {
+//           done();
+//         });
+//       }
+
+//       var data = require('fs').createReadStream('./test/test.tar');
+//       docker.buildImage(data, {}, handler);
+//     });
+//   });
+
+//   describe("#getEvents", function() {
+//     it("should get events", function(done) {
+//       this.timeout(30000);
+
+//       function handler(err, stream) {
+//         expect(err).to.be.null;
+//         expect(stream).to.be.ok;
+//         //stream.pipe(process.stdout, {end: true});
+//         done();
+//       }
+
+//       docker.getEvents({since: ((new Date().getTime()/1000) - 60).toFixed(0)}, handler);
+//     });
+//   });
+
+//   describe("#getPing", function() {
+//     it("should ping server", function(done) {
+//       this.timeout(30000);
+
+//       function handler(err, data) {
+//         expect(err).to.be.null;
+//         done();
+//       }
+
+//       docker.ping(handler);
+//     });
+//   });
+
+//   describe("#testTimeout", function() {
+//     it("should timeout", function(done) {
+//       this.timeout(30000);
+
+//       function handler(err, data) {
+//         expect(err).to.not.be.null;
+//         done();
+//       }
+
+//       dockert.searchImages({term: 'node'}, handler);
+//     });
+//   });
+
+//   describe('#pull', function() {
+//     this.timeout(120000);
+
+//     // one image with one tag
+//     var repoTag = testImage;
+
+//     function locateImage(image, callback) {
+//       docker.listImages(function(err, list) {
+//         if (err) return callback(err);
+
+//         // search for the image in the RepoTags
+//         var image;
+//         for (var i = 0, len = list.length; i < len; i++) {
+//           if (list[i].RepoTags.indexOf(repoTag) !== -1) {
+//             // ah ha! repo tags
+//             return callback(null, docker.getImage(list[i].Id));
+//           }
+//         }
+
+//         return callback();
+//       });
+//     }
+
+//     /*
+//     beforeEach(function(done) {
+//       locateImage(repoTag, function(err, image) {
+//         if (err) return done(err);
+//         if (image) return image.remove(done);
+//         done();
+//       });
+//     });
+//     */
+
+//     it('should pull image from remote source', function(done) {
+//       function handler() {
+//         locateImage(repoTag, function(err, image) {
+//           if (err) return done(err);
+//           // found the image via list images
+//           expect(image).to.be.ok;
+//           done();
+//         });
+//       }
+
+//       docker.pull(repoTag, function(err, stream) {
+//         if (err) return done(err);
+//         stream.pipe(process.stdout);
+//         stream.once('end', handler);
+//       });
+//     });
+
+//     it('should pull image from remote source using followProgress', function(done) {
+//       docker.pull(repoTag, function(err, stream) {
+//         if (err) return done(err);
+//         docker.modem.followProgress(stream, onFinished, onProgress);
+
+//         function onFinished(err, output) {
+//           //ouput is an array of objects, already json parsed.
+//           if (err) return done(err);
+//           expect(output).to.be.a('array');
+//           done();
+//         }
+
+//         function onProgress(event) {
+//           expect(event).to.be.ok;
+//         }
+//       });
+//     });
+
+//     it('should pull image from remote source using followProgress and firing only in the end', function(done) {
+//       docker.pull(repoTag, function(err, stream) {
+//         if (err) return done(err);
+//         docker.modem.followProgress(stream, onFinished);
+
+//         function onFinished(err, output) {
+//           //ouput is an array of objects, already json parsed.
+//           if (err) return done(err);
+//           expect(output).to.be.a('array');
+//           done();
+//         }
+//       });
+//     });
+
+//     it('should pull image from remote source using followProgress and pause', function(done) {
+//       docker.pull(repoTag, function(err, stream) {
+//         if (err) return done(err);
+//         docker.modem.followProgress(stream, onFinished, onProgress);
+
+//         function onFinished(err, output) {
+//           if (err) return done(err);
+//           expect(output).to.be.a('array');
+//         }
+
+//         function onProgress(event) {
+//           expect(event).to.be.ok;
+//           done();
+//           stream.destroy();
+//         }
+//       });
+//     });
+//   });
+
+//   describe("#run", function() {
+//     this.timeout(30000);
+
+//     it("should emit partial data", function(done) {
+//       function handler(err, data, container) {
+//         expect(err).to.be.null;
+//         //container is created
+//         expect(container).to.be.ok;
+
+//         container.remove(function(err, data) {
+//           expect(err).to.be.null;
+//         });
+//       }
+
+//       var ee = docker.run(testImage, ['bash', '-c', 'uname -a'], process.stdout, handler);
+//       ee.on('container', function (container) {
+//         expect(container).to.be.ok;
+//       });
+//       ee.on('stream', function (stream) {
+//         expect(stream).to.be.ok;
+//       });
+//       ee.on('data', function (data) {
+//         expect(data).to.be.ok;
+//         done();
+//       });
+//     });
+
+//     it("should run a command", function(done) {
+//       function handler(err, data, container) {
+//         expect(err).to.be.null;
+//         //container is created
+//         expect(container).to.be.ok;
+
+//         container.remove(function(err, data) {
+//           expect(err).to.be.null;
+//           done();
+//         });
+//       }
+
+//       docker.run(testImage, ['bash', '-c', 'uname -a'], process.stdout, handler);
+//     });
+
+//     it("should run a command with start options", function(done){
+//       function handler(err, data, container) {
+//         expect(err).to.be.null;
+
+//         container.inspect(function(err, data){
+//           expect(err).to.be.null;
+//           expect(data.HostConfig.Privileged).to.be.true;
+
+//           container.remove(function(err, data){
+//             expect(err).to.be.null;
+//             done();
+//           });
+//         });
+//       }
+//       docker.run(testImage, ['bash', '-c', 'uname -a'], process.stdout, {}, {Privileged : true}, handler);
+//     });
+
+//     it("should run a command with create options", function(done){
+//       function handler(err, data, container) {
+//         expect(err).to.be.null;
+
+//         container.inspect(function(err, data){
+//           expect(err).to.be.null;
+
+//           container.remove(function(err, data){
+//             expect(err).to.be.null;
+//             done();
+//           });
+//         });
+//       }
+//       docker.run(testImage, ['bash', '-c', 'uname -a'], process.stdout, {}, handler);
+//     });
+//   });
+
+//   describe("#createContainer", function() {
+//     it("should create and remove a container", function(done) {
+//       this.timeout(5000);
+
+//       function handler(err, container) {
+//         expect(err).to.be.null;
+//         expect(container).to.be.ok;
+
+//         container.inspect(function (err, info) {
+//           expect(err).to.be.null;
+//           expect(info.Name).to.equal('/test');
+
+//           container.remove(function(err, data) {
+//             expect(err).to.be.null;
+//             done();
+//           });
+//         });
+//       }
+
+//       docker.createContainer({Image: testImage, Cmd: ['/bin/bash'], name: 'test'}, handler);
+//     });
+//   });
+
+//   describe("#createImage", function() {
+//     it("should create an image", function(done) {
+//       this.timeout(120000);
+
+//       function handler(err, stream) {
+//         expect(err).to.be.null;
+//         expect(stream).to.be.ok;
+
+//         stream.pipe(process.stdout, {end: true});
+
+//         stream.on('end', function() {
+//           done();
+//         });
+//       }
+
+//       docker.createImage({fromImage: testImage}, handler);
+//     });
+//   });
+
+//   describe("#listContainers", function() {
+//     it("should list containers", function(done) {
+//       this.timeout(5000);
+
+//       function handler(err, data) {
+//         expect(err).to.be.null;
+//         expect(data).to.be.a('array');
+//         done();
+//       }
+
+//       docker.listContainers({all: 1}, handler);
+//     });
+//   });
+
+//   describe("#listImages", function() {
+//     it("should list images", function(done) {
+//       this.timeout(5000);
+
+//       function handler(err, data) {
+//         expect(err).to.be.null;
+//         expect(data).to.be.a('array');
+//         done();
+//       }
+
+//       docker.listImages({all: 1}, handler);
+//     });
+//   });
+
+//   describe("#version", function() {
+//     it("should return version", function(done) {
+//       this.timeout(5000);
+
+//       function handler(err, data) {
+//         expect(err).to.be.null;
+//         expect(data).to.be.ok;
+//         done();
+//       }
+
+//       docker.version(handler);
+//     });
+//   });
+
+//   describe("#searchImages", function() {
+//     it("should return search results", function(done) {
+//       this.timeout(120000);
+
+//       function handler(err, data) {
+//         expect(err).to.be.null;
+//         expect(data).to.be.a('array');
+//         done();
+//       }
+
+//       docker.searchImages({term: 'node'}, handler);
+//     });
+//   });
+
+//   describe("#info", function() {
+//     it("should return system info", function(done) {
+//       this.timeout(5000);
+
+//       function handler(err, data) {
+//         expect(err).to.be.null;
+//         expect(data).to.be.ok;
+//         done();
+//       }
+
+//       docker.info(handler);
+//     });
+//   });
+// });
---
+++ test-shipyard/spec_helper.js
@@ -0,0 +1,32 @@
+var http = require("http");
+http.globalAgent.maxSockets = 1000;
+
+var Docker = require('../lib/docker');
+var fs     = require('fs');
+
+// For Mac OS X:
+// socat -d -d unix-l:/tmp/docker.sock,fork tcp:<docker-host>:4243
+// DOCKER_SOCKET=/tmp/docker.sock npm test
+
+var socket   = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
+var isSocket = fs.existsSync(socket) ? fs.statSync(socket).isSocket() : false;
+var docker;
+
+var tcphost="http://192.168.5.152";
+var tcpport=8081;
+var serviceKey="7ER9.xwdkdrVzrN.UEb1rBaRGR.1rnRJOqFe"
+
+if (!isSocket) {
+  console.log('Trying TCP connection... host is '+tcphost +' post is'+tcpport +' service key is '+serviceKey);
+  
+  docker = new Docker({host:tcphost,port:tcpport,serviceKey:serviceKey});
+  dockert = new Docker({host:tcphost,port:tcpport,timeout: 1,serviceKey:serviceKey});
+} else {
+  docker = new Docker({ socketPath: socket });
+  dockert = new Docker({ socketPath: socket, timeout: 1 });
+}
+
+module.exports = {
+  'docker': docker,
+  'dockert': dockert
+};
